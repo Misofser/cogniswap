415438027200be7aa06d34b66951a65fb4df7ec6
Sofia <misofser@gmail.com>
2023-05-09 09:20:52 +0300

    Create input fields for user registration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    study: '',
+    teach: ''
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleInputChange}
+        />
+      </label>
+
+      <label>
+        Date of birth:
+        <input
+          type="date"
+          name="age"
+          value={formData.age}
+          onChange={handleInputChange}
+        />
+      </label>
+
+      <label>
+        What do you want to study?
+        <input
+          type="text"
+          name="study"
+          value={formData.study}
+          onChange={handleInputChange}
+        />
+      </label>
+
+      <label>
+        What are you ready to teach?
+        <input
+          type="text"
+          name="teach"
+          value={formData.teach}
+          onChange={handleInputChange}
+        />
+      </label>
+
+      <p>Are you ready to start?</p>
+      <button type="submit">Yes</button>
+    </form>
   );
 }
 
