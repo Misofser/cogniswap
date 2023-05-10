3623e9a17dc2e33664fefac1ed8c310c843872d0
Sofia <misofser@gmail.com>
2023-05-10 08:07:06 +0300

    Apply styling to the registration form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   const [formData, setFormData] = useState({
@@ -22,50 +23,56 @@ function App() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-      </label>
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <label className="form-label">
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </label>
 
-      <label>
-        Date of birth:
-        <input
-          type="date"
-          name="age"
-          value={formData.age}
-          onChange={handleInputChange}
-        />
-      </label>
+        <label className="form-label">
+          Date of birth:
+          <input
+            type="date"
+            name="age"
+            value={formData.age}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </label>
 
-      <label>
-        What do you want to study?
-        <input
-          type="text"
-          name="study"
-          value={formData.study}
-          onChange={handleInputChange}
-        />
-      </label>
+        <label className="form-label">
+          What do you want to study?
+          <input
+            type="text"
+            name="study"
+            value={formData.study}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </label>
 
-      <label>
-        What are you ready to teach?
-        <input
-          type="text"
-          name="teach"
-          value={formData.teach}
-          onChange={handleInputChange}
-        />
-      </label>
+        <label className="form-label">
+          What are you ready to teach?
+          <input
+            type="text"
+            name="teach"
+            value={formData.teach}
+            onChange={handleInputChange}
+            className="form-input"
+          />
+        </label>
 
-      <p>Are you ready to start?</p>
-      <button type="submit">Yes</button>
-    </form>
+        <p>Are you ready to start?</p>
+        <button type="submit" className="form-submit">Yes</button>
+      </form>
+    </div>
   );
 }
 
