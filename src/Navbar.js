e9b248d5e09597875fdbadfbd7fdf9dd74160afd
Sofia <misofser@gmail.com>
2023-05-13 09:55:36 +0300

    Add app name and some style to nav bar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav>
+    <nav className="navbar">
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
       </ul>
+      <div className="app-name">CogniSwap</div>
     </nav>
   );
 }
